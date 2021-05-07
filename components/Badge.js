4539a210c20dfc059a6ed1db1af75ccc759ff6f0
Jack Steam <jacksteamdev@gmail.com>
2021-05-07 11:10:15 -0500

    fix docs links

--- components/Badge.js
+++ components/Badge.js
@@ -1,7 +1,14 @@
-const Badge = ({ alt, href = '#', src }) => (
-  <a className="inline-block mr-1" href={href}>
-    <img alt={alt} src={src} />
-  </a>
-)
+const isDefined = (x) => typeof x !== 'undefined'
+
+const Badge = ({ alt, href, src }) =>
+  isDefined(href) ? (
+    <a className="inline-block mr-1" href={href}>
+      <img alt={alt} src={src} />
+    </a>
+  ) : (
+    <div className="inline-block mr-1">
+      <img alt={alt} src={src} />
+    </div>
+  )
 
 export default Badge
