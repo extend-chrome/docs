8028714807b4fd6d6bfcff6914c8ee5144fd367d
Jack Steam <jacksteamdev@gmail.com>
2021-11-08 14:25:58 -0600

    feat: add posthog analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import 'nextra-theme-docs/style.css'
 import Prism from 'prism-react-renderer/prism'
+import posthog from 'posthog-js'
 ;(typeof global !== 'undefined' ? global : window).Prism = Prism
+
+posthog.init('phc_dNxpLX1NPBg8qJPd4HycpkGW9cXBNo8JL1buSTtw1ld', {
+  api_host: 'https://app.posthog.com',
+})
 
 require('prismjs/components/prism-kotlin')
 require('prismjs/components/prism-csharp')
