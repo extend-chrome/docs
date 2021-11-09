26593f573533515bce9f6d5b3891153b0c04f404
Jack Steam <jacksteamdev@gmail.com>
2021-11-09 11:17:11 -0600

    feat: add pageview capture on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import 'nextra-theme-docs/style.css'
 import Prism from 'prism-react-renderer/prism'
 import posthog from 'posthog-js'
+import { useRouter } from 'next/router'
 ;(typeof global !== 'undefined' ? global : window).Prism = Prism
+import { useEffect } from 'react'
 
-if (process.browser)
+if (process.browser && !window.location.href.includes('localhost'))
   posthog.init('phc_dNxpLX1NPBg8qJPd4HycpkGW9cXBNo8JL1buSTtw1ld', {
     api_host: 'https://app.posthog.com',
   })
@@ -11,6 +13,18 @@ if (process.browser)
 require('prismjs/components/prism-kotlin')
 require('prismjs/components/prism-csharp')
 
+function handleRouteChange() {
+  posthog.capture('$pageview')
+}
+
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
